ac9a8892f52701df39ac20c8af145ded8241864b
1FanDuan <949215869@qq.com>
2023-02-08 17:27:39 +0800

    Initial commit

---
+++ src/main/java/tacos/controller/HomeController.java
@@ -0,0 +1,12 @@
+package tacos.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @RequestMapping("/")
+    public String home(){
+        return "home";
+    }
+}
