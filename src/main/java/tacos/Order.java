98611299a896a21350e8512e3418b9e7930490c6
1FanDuan <949215869@qq.com>
2023-02-15 17:31:16 +0800

    new version

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -3,22 +3,35 @@ package tacos;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
-public class Order {
-    @NotBlank(message = "Name is required")
-    private String name;
+@Entity
+@Table(name = "Taco_Order")
+public class Order implements Serializable {
+    private static final long serivalVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Date placedAt;
+    @NotBlank(message = "Delivery Name is required")
+    private String deliveryName;
     @NotBlank(message = "Street is required")
-    private String street;
+    private String deliveryStreet;
     @NotBlank(message = "City is required")
-    private String city;
+    private String deliveryCity;
     @NotBlank(message = "State is required")
-    private String state;
+    private String deliveryState;
     @NotBlank(message = "Zip code is required")
-    private String zip;
+    private String deliveryZip;
     @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
     @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
@@ -26,5 +39,16 @@ public class Order {
     private String ccExpiration;
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
+    @ManyToMany(targetEntity = Taco.class)
+    private List<Taco> tacos = new ArrayList<>();
+
+    public void addDesign(Taco design) {
+        this.tacos.add(design);
+    }
+
+    @PrePersist
+    void placedAt(){
+        this.placedAt = new Date();
+    }
 
 }
